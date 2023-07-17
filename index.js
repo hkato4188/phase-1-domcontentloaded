a75b4e7b2b34a012c66a8288f5940f102166ad2c
Hiro Kato <hirokikato1@gmail.com>
2023-07-17 13:46:33 -0400

    completse lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("locked and loaded!")
+    updateDOM()
+})
+
+function updateDOM(){
+    let paragraph = document.getElementById('text')
+    paragraph.textContent = "This is really cool!"
+}
